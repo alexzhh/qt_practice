f26deafab9d348e83eb2dd79da406b8eeb705eac
DeFeiXu <xdf900125261@126.com>
2014-04-09 20:03:17 +0800

    dcm2xml successful

--- develop/savefile.cpp
+++ develop/savefile.cpp
@@ -9,6 +9,7 @@
 #include "dcminformation.h"
 #include <iostream>
 #include <QFile>
+#include <QProcess>
 
 savefile::savefile(QWidget *parent) :
     QWidget(parent)
@@ -28,20 +29,20 @@ bool savefile::saveFile(QString fileName,QString openfilepath)
     doc.getDcmData().saveFile(fileName.toStdString().c_str());
     if(fileName.endsWith(".xml"))
     {
-        DcmFileFormat fileFormat;
-        DcmMetaInfo     fileMetaIfo;
-        DcmDataset          fileDataset;
-        std::ostream& fileStream=COUT;
-        QXmlStreamReader xmlReader;
+///*        DcmFileFormat fileFormat;
+//        DcmMetaInfo     fileMetaIfo;
+//        DcmDataset          fileDataset*/;
+//        std::ostream& fileStream=COUT;
+//        QXmlStreamReader xmlReader;
 
-        QFile file(fileName.toStdString().c_str());
-        if(!file.open(QIODevice::ReadWrite))
-            return false;
+//        QFile file(fileName.toStdString().c_str());
+//        if(!file.open(QIODevice::ReadWrite))
+//            return false;
 
 
-        QXmlStreamWriter xmlWriter(&file);
-        xmlWriter.setAutoFormatting(true);
-    QString out;
+//        QXmlStreamWriter xmlWriter(&file);
+//        xmlWriter.setAutoFormatting(true);
+//        QString out;
 //       QFile file(fileName);
 //        if(!file.open(QIODevice::WriteOnly | QIODevice::Text))
 //        {
@@ -52,11 +53,17 @@ bool savefile::saveFile(QString fileName,QString openfilepath)
         //qDebug()<<"<?xml version=\"1.0\" encoding=\"windows-1252\"?>"\
            //    <<fileFormat.writeXML()
           //   <<dcmMe.writeXML(fileStream.write(openfilepath.toStdString().c_str(),std::ifstream::binary)).text()<<"++++++++++++++++++++++++++++++";
-         out=doc.getDcmData().writeXML(fileStream.write(openfilepath.toStdString().c_str(),std::ifstream::binary)).text();
-        xmlWriter.writeStartDocument();
-        xmlWriter.writeStartElement("sknd");
-        xmlWriter.writeEndDocument();
-
+//        out= doc.getDcmData().writeXML(fileStream).text();
+//         qDebug()<<tr("xnclk");
+ //       xmlWriter.writeStartDocument();
+//        xmlWriter.writeStartElement("sknd");
+//        xmlWriter.writeEndDocument();
+        QStringList list1;
+    //    QStringList list2;
+        list1<<"+f"<<openfilepath<<fileName;
+    //    list2<<"Xf"<<fileName;
+        QProcess::startDetached(tr("D:\\Qt\\DCMTK\\bin\\dcm2xml.exe"),list1);
+   //     QProcess::startDetached(tr("D:\\Qt\\DCMTK\\bin\\dcm2xml.exe"),list2);
 
     }
     }
