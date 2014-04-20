f7f08847dfb042ae754048bfd24c540ef06e6e1d
zhouwanchun1993 <zhouwanchun1993@163.com>
2014-04-20 23:01:25 +0800

    Add commit

--- develop/mainwindow.h
+++ develop/mainwindow.h
@@ -26,20 +26,34 @@ private:
     Ui::MainWindow *ui;
     QVector<DcmElement*> FilePatientInfo;
     DcmInformation* dcm;
+    //all user input flag,0 if ok,nonzero otherwise
     unsigned long InputStatu;
 public:
+    //Paint DCM image
     void PaintDCM(QPixmap &DCMPix);
 public slots:
+    //Choose a file from local disk to open,return Filepath selected
     QString SelectFile();
+    //init dcmobject(free space)
     void InitDCMObject(DcmInformation* dcmObject);
+    //pass Filepath to construct dcm object
     bool LoadFile();
+    //interface ,Save as another file format
     void SaveFile();
+    //Exit Windows
     void QuitWindows();
+    //when Edit menu checked,user can rewrite patient info,
+    //otherwise,it would be read only
     void EditModeChanged(bool);
+    //display Patient informatin
     void FillPatientInfo(PatientInfo Type,QString ValueFiled);
+    //Write Patient Information(modified by user) to File
     void SavePatientInfo2File();
+    //Use default Patient information
     void ResetPatientInfo();
+    //Alert while error input
     void UpdataErrorInfo();
+    //validate user input,true if ok, false otherwise
     bool CheckDataValid(PatientInfo VRType,const QString Value);
 };
 #endif // MAINWINDOW_H
