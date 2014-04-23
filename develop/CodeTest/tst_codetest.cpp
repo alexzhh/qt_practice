2befd3cac95a6d78663da13655b9e960159b5415
StudentQI <413380892@qq.com>
2014-04-23 21:41:30 +0800

    Update Error test code

--- develop/CodeTest/tst_codetest.cpp
+++ develop/CodeTest/tst_codetest.cpp
@@ -13,12 +13,13 @@ private:
     DcmInformation *testDcm;
 
 private Q_SLOTS:
+    //正确测试
     void testStructure();
     void cleanupTestCase();
     void testloadFromDCM();
     void testgetStringlizeTag();
     void testendOfDataSet();
-    void testisisValid();
+    void testisValid();
     void testcustomSaveFile();
     void testgetSavedFileType();
     void testsaveDcmFile();
@@ -31,6 +32,28 @@ private Q_SLOTS:
     void testdrawDcmImage();
     void testputAndInsertString();
     void testcheckEachTag();
+
+    //以下为错误测试,由于错误测试时赋值为错误的，导致dcminformation无法初始化，许多方法无法调用
+    void testStructureFalse();
+    void testloadFromDCMFalse();
+    void testElementIsExist();
+    //isValid()函数存在问题，初始化失败后还可以继续调用，会报异常，预期为返回false
+    //void testisValidFalse();
+    //void testcustomSaveFileFalse();
+    //void testgetSavedFileTypeFalse();
+    //void testsaveDcmFileFalse();
+    //void testsaveXmlFileFalse();
+    // void testgetAttributesFalse();
+    //void testgetAttributesFalse_data();
+    void testsetOutputFileFalse();
+    void testgetInputFileFalse();
+    void testgetOutputFileFalse();
+    //void testdrawDcmImageFalse();
+    //void testputAndInsertStringFalse();
+    void testcheckEachTagFalse();
+
+
+
 };
 
 //构造和回收函数
@@ -46,6 +69,8 @@ void CodeTest::cleanupTestCase()
 
 //测试函数的实现
 
+
+
 void CodeTest::testStructure()
 {
     QString testiPath="e://CT.dcm";
@@ -54,6 +79,7 @@ void CodeTest::testStructure()
     QCOMPARE(testDcm->getInputFile(),QString("e://CT.dcm"));
     QCOMPARE(testDcm->getOutputFile(),QString(""));
 
+
 }
 
 
@@ -61,6 +87,7 @@ void CodeTest::testloadFromDCM()
 {
 
    QCOMPARE(testDcm->loadFromDCM(),true);
+
 
 
 }
@@ -73,6 +100,7 @@ void CodeTest::testgetStringlizeTag()
       testDcm->getDataset()->findAndGetElement(DCM_PatientName,testDcmElement);
       QString testString=QString(testDcm->getStringlizeTag(testDcmElement->getTag()));
       QCOMPARE(testString,QString("(0010,0010)"));
+
 }
 
 void CodeTest::testendOfDataSet()
@@ -81,18 +109,20 @@ void CodeTest::testendOfDataSet()
     testDcm->getDataset()->findAndGetElement(DCM_PatientName,testDcmElement);
     QCOMPARE(testDcm->endOfDataSet(testDcmElement->getTag()),false);
 
+
 }
 
 
 
-void CodeTest::testisisValid()
+void CodeTest::testisValid()
 {
 
     QCOMPARE(testDcm->isValid(),true);
 
+
 }
 
-//暂定测试
+//暂不测试
 void CodeTest::testcustomSaveFile()
 {
 
@@ -111,7 +141,7 @@ void CodeTest::testsetOutputFile()
 {
     QString testString="e://1.dcm";
     testDcm->setOutputFile(testString);
-    QCOMPARE(testDcm->getOutputFile(),testString);
+    QCOMPARE(testDcm->getOutputFile(),QString("e://1.dcm"));
 
 
 }
@@ -187,9 +217,9 @@ void CodeTest::testdrawDcmImage()
 
 void CodeTest::testputAndInsertString()
 {
-    //这里输入了正确的值与错误的对比了
+    //这里输入了正确的值与正确的对比了
     OFString testvalue_tmp;
-    QString testString="ABC";
+    QString testString="GGGGG AAAAA BBBB";
     //testDcm->putAndInsertString(0x0010,0x0010,testString);
     testDcm->getDataset()->findAndGetOFString(DCM_PatientName ,testvalue_tmp);
     //QCOMPARE(QString(testvalue_tmp.c_str()),testString);
@@ -211,8 +241,88 @@ void CodeTest::testcheckEachTag()
 
 }
 
+//以下为错误测试函数的实现
+void CodeTest::testStructureFalse()
+{
+    delete testDcm;
+    QString testiPath="testvalue";
+    QString testoPath="";
+    testDcm=new DcmInformation(testiPath,testoPath);
+    QCOMPARE(testDcm->getInputFile(),QString("e://CT.dcm"));
+    QCOMPARE(testDcm->getOutputFile(),QString(""));
+
+}
+
+void CodeTest::testloadFromDCMFalse()
+{
+
+   QCOMPARE(testDcm->loadFromDCM(),true);
 
 
+
+}
+
+void CodeTest::testElementIsExist()
+{
+      DcmElement *testDcmElement;
+      bool testboolvalue;
+      if(testDcm->getDataset()->findAndGetElement(DCM_PatientName,testDcmElement).bad())
+      {
+
+          testboolvalue=false;
+
+      }
+      QCOMPARE(testboolvalue,true);
+
+}
+
+/*
+void CodeTest::testisValidFalse()
+{
+
+    bool testboolvalue=testDcm->isValid();
+    QCOMPARE(testboolvalue,true);
+
+
+}
+
+*/
+
+void CodeTest::testsetOutputFileFalse()
+{
+    QString testString="TestValue";
+    testDcm->setOutputFile(testString);
+    QCOMPARE(testDcm->getOutputFile(),QString("e://1.dcm"));
+
+
+}
+
+
+void CodeTest::testgetInputFileFalse()
+{
+
+   QCOMPARE(testDcm->getInputFile(),QString("e://CT.dcm"));
+
+}
+
+void CodeTest::testgetOutputFileFalse()
+{
+
+   QCOMPARE(testDcm->getOutputFile(),QString("e://1.dcm"));
+
+}
+
+void CodeTest::testcheckEachTagFalse()
+{
+
+    int testintvalue;
+    const int & testvalue=testintvalue;
+    testintvalue=26;
+    bool testboolvalue=testDcm->checkEachTag(testvalue,NULL);
+    QCOMPARE(testboolvalue,true);
+
+
+}
 QTEST_APPLESS_MAIN(CodeTest)
 
 #include "tst_codetest.moc"
